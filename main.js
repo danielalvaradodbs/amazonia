f7bad33ce808e0775442c709cc278d4e8b3f20e1
miguel-dbscol <miguelbaron@dbscol.com>
2023-07-19 14:56:23 -0500

    2do y 3er mapa

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-// Imagenes //
-
-//const imgBasePA = document.querySelector('.imgBasePA');
+/* Imagenes */
 const imgMineriaPA = document.querySelector('.imgMineriaPA');
 const imgPetroleoPA = document.querySelector('.imgPetroleoPA');
 const imgHidroelectricaPA = document.querySelector('.imgHidroelectricaPA');
 const imgDeforestacioPA = document.querySelector('.imgDeforestacioPA');
 
-/* inputs */
+const imgTerritoriosTA = document.querySelector('.imgTerritoriosTA');
+const imgAreasTA = document.querySelector('.imgAreasTA');
 
+const imgRiosTI = document.querySelector('.imgRiosTI');
+const imgResguardosTI = document.querySelector('.imgResguardosTI');
+const imgAreasTI = document.querySelector('.imgAreasTI');
+const imgTerritoriosTI = document.querySelector('.imgTerritoriosTI');
+
+/* inputs */
 const mineriaPA = document.querySelector('.mineriaPA');
 const petroleoPA = document.querySelector('.petroleoPA');
 const hidroelectricaPA = document.querySelector('.hidroelectricaPA');
 const deforestacioPA = document.querySelector('.deforestacioPA');
+
+const territoriosTA = document.querySelector('.territoriosTA');
+const areasTA = document.querySelector('.areasTA');
+
+const riosTI = document.querySelector('.riosTI');
+const resguardosTI = document.querySelector('.resguardosTI');
+const areasTI = document.querySelector('.areasTI');
+const territoriosTI = document.querySelector('.territoriosTI');
+
 
 const imagenVisible = ( input, image ) => {
 
@@ -34,4 +48,12 @@ imagenVisible(mineriaPA, imgMineriaPA);
 imagenVisible(petroleoPA, imgPetroleoPA);
 imagenVisible(hidroelectricaPA, imgHidroelectricaPA);
 imagenVisible(deforestacioPA, imgDeforestacioPA);
+
+imagenVisible(territoriosTA, imgTerritoriosTA);
+imagenVisible(areasTA, imgAreasTA);
+
+imagenVisible(riosTI, imgRiosTI);
+imagenVisible(resguardosTI, imgResguardosTI);
+imagenVisible(areasTI, imgAreasTI);
+imagenVisible(territoriosTI, imgTerritoriosTI);
 
